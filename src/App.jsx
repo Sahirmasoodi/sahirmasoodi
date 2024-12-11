22da794e804813e8f67e7d056186b4d00fdd5097
Sahir ul islam <sahir.masoodi@gmail.com>
2024-12-11 22:44:07 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ const App = () => {
       <BrowserRouter>
         <Navbar/>
         <Routes>
-          <Route path="/portfolio" element={<Home/>} />
+          <Route path="/sahirmasoodi" element={<Home/>} />
           <Route path="/about" element={<About/>} />
           <Route path="/projects" element={<Projects/>} />
           <Route path="/contact" element={<Contact/>} />
