358007b02ad38f60fe1d4463d48bdd5936ca825b
Sahirmasoodi <153169842+Sahirmasoodi@users.noreply.github.com>
2024-12-12 18:17:39 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ const App = () => {
       <BrowserRouter>
         <Navbar/>
         <Routes>
-          <Route path="/sahirmasoodi" element={<Home/>} />
+          <Route index element={<Home/>} />
           <Route path="/about" element={<About/>} />
           <Route path="/projects" element={<Projects/>} />
           <Route path="/contact" element={<Contact/>} />
